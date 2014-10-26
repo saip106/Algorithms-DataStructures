1e65fc64b1561455bd899d6f92056559ad18b9c0
Sai Gudigundla <saip106@msn.com>
2014-10-25 20:53:25 -0400

    implemented quick find

--- app/dynamic-connectivity/quick-find-spec.js
+++ app/dynamic-connectivity/quick-find-spec.js
@@ -30,10 +30,36 @@ describe('when checking to see if two disconnected points are connected', functi
 
 describe('when checking to see if two connected points are connected', function () {
 
-    it('should find the connection', function () {
-        var problemSet = new quickFind.ProblemSet();
+    var problemSet;
+    beforeEach(function () {
+        problemSet = new quickFind.ProblemSet();
         problemSet.initialize(5);
         problemSet.connect(0, 1);
+    });
+
+    describe('when the connected points have other connections', function () {
+
+        beforeEach(function () {
+            problemSet.connect(0, 2);
+            problemSet.connect(0, 3);
+            problemSet.connect(1, 4);
+            problemSet.connect(1, 5);
+        });
+
+        it('should find the connection', function () {
+            assert.strictEqual(problemSet.areConnected(0, 1), true);
+        });
+
+        it('should maintain the new connections', function () {
+            assert.strictEqual(problemSet.areConnected(0, 2), true);
+            assert.strictEqual(problemSet.areConnected(0, 3), true);
+            assert.strictEqual(problemSet.areConnected(1, 4), true);
+            assert.strictEqual(problemSet.areConnected(1, 5), true);
+        });
+
+    });
+
+    it('should find the connection', function () {
 
         var result = problemSet.areConnected(0, 1);
         assert.strictEqual(result, true);
